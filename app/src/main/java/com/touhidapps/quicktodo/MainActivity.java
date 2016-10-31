2abb4198d6cfc63fd078436ff42c1c84927a022a
touhid <touhid1010@gmail.com>
2016-11-01 00:22:07 +0600

    nnn

--- app/src/main/java/com/touhidapps/quicktodo/MainActivity.java
+++ app/src/main/java/com/touhidapps/quicktodo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.touhidapps.quicktodo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.touhidapps.quicktodo.login.LoginActivity;
 import com.touhidapps.quicktodo.login.LoginSession;
@@ -25,10 +30,60 @@ public class MainActivity extends AppCompatActivity {
         fab_addGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // todo
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(getApplicationContext());
+                View promptsView = li.inflate(R.layout.my_prompts, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        getApplicationContext());
+
+                // set my_prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+
+                                        Toast.makeText(MainActivity.this, ""+userInput.getText(), Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
             }
         });
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } // End of OnCreate
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
